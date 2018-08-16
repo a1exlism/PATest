2bb5e9c50215061849c09bbdebcb0b7b89fc30e5
a1exlism <shengjitian@gmail.com>
2018-08-16 13:23:33 +0800

    B1009; fgets usage

--- Algorithm-line/chapter-3/B1009.cpp
+++ Algorithm-line/chapter-3/B1009.cpp
@@ -1,9 +1,50 @@
 /**
- *  说反话???
+ *  说反话
+ *  words reverse
+ *          ATTENTION: while 能不用就不用, 坑爹
+ *          2018+ : gets函数无法使用, 改用fgets
  **/
 
 #include <cstdio>
-int main() {
+#include <cstring>
+int main()
+{
+  char str[500];    //  既定内存可以更大, 一开始的100提示partitial accept
+  char str_arr[50][50];
+  int j = 0, k = 0;
+  // gets(str);
+  fgets(str, 500, stdin);
+  str[strlen(str)-1] = '\0';
+
+  for(int i = 0; str[i] != '\0'; i++) {
+    if(str[i] != ' ') {
+      str_arr[j][k++] = str[i];
+    } else {
+      //  TODO: Close the string
+      str_arr[j][k] = '\0';
+      //  ATTENTION: Re-initial
+      j++;
+      k = 0;
+    }
+  }
+
+  /** BAD circulation **/
+  // while(str[i] != '\0') {
+  //   if(str[i] == ' ') {
+  //     str_arr[j][k] = '\0';
+  //     j++;
+  //     k = 0;
+  //     i++;
+  //   } else {
+  //     str_arr[j][k++] = str[i++];
+  //   }
+  // }
+
+
+  while(j > 0) {
+    printf("%s ", str_arr[j--]);
+  }
+  printf("%s", str_arr[j]);
 
   return 0;
 }
